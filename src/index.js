c6059275a5a6170a03bb3e67b7303e63e7ee4bc6
Alexsander Brantsevich <lexsander.red.34@gmail.com>
2020-03-23 22:33:46 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 
 exports.min = function min (array) {
-  if (!array || !array.length) return 0;
-  return Math.min(...array);
+  if (array == null || array == false || array == undefined || array.length == undefined) {
+      return 0;
+  }
+ let min
+  for(let i=0; i<array.length; i++) {
+      if (i === 0) {
+          min = array[i]
+      } else if(array[i]<min){
+          min = array[i]
+      }
+  }
+  return min
 }
 
 exports.max = function max (array) {
-   if (!array || !array.length) return 0;
-   return Math.max(...array);
+  if (array == null || array == false || array == undefined || array.length == undefined) {
+      return 0;
+  }
+  let max
+  for(let i=0; i<array.length; i++) {
+      if (i === 0) {
+          max = array[i]
+      } else if(array[i]>max){
+          max = array[i]
+      }
+  }
+  return max
 }
 
 exports.avg = function avg (array) {
-    if (!array || !array.length) return 0;
-    return array.reduce((acu, cur) => acu += cur, 0)/array.length;
+  if (array == null || array == false || array == undefined || array.length == undefined) {
+      return 0;
+  }
+  let num = array.length
+  let newArr = array.reduce((accum, next) => accum+next)
+  return newArr/num
 }
